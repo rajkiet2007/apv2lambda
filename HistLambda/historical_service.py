37786913b60c0fd06fbd670544b1552252b972d3
unknown <rawsexpert@gmail.com>
2022-11-22 23:00:28 +0530

    Add appspec file

--- HistLambda/historical_service.py
+++ HistLambda/historical_service.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello Historical Lambda Updated!')
+        'body': json.dumps('New Code Added for Historical Lambda!')
     }
